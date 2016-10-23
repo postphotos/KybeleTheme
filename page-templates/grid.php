07aca3ad339f8786ff4bad5262c9f765cc86d286
Leo Postovoit <lpostovoit@gmail.com>
2016-10-23 00:05:16 -0700

    cleaning up headers, making templates make sense and so on. ALSO, WICKED SCREENSHOT

--- page-templates/grid.php
+++ page-templates/grid.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Template Name: Main
+Template Name: Grid
 */
 get_header(); ?>
 
@@ -17,16 +17,13 @@ if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned
 				top: 0;
 				left: 0;
 				z-index: -1;
-				width: 100%; 
 			}
 			.background {
 				z-index: -1;
-				height: 100%;
-				width: 100%;
+				width: auto;
+				max-width: 100%;
 				top: 0;
-				clip: auto;
 				left: 0;
-				object-fit: cover;
 			}
 			.page {
 				z-index: 2;
@@ -35,23 +32,15 @@ if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned
 				background: rgba(255,255,255,0.6);
 				height: 100%;
 			}
-
-			.bkg-image {
-				background-size:cover;
-				height: 999px;
-				width: 100%;
-				position: absolute;
-				top: 0;
-				left: 0;
-				z-index: -1;
-			}
-	</style> <?php
+		</style> <?php
 } 
 ?>
+
+
 		<div id="content" role="main">
-				<div class="bkg-image" style="background-image: url('<?php the_post_thumbnail_url(); ?>');" alt="Image" />
-					<br />
-				</div>
+			<div class="background-wrapper">
+				<img class="background" src="<?php the_post_thumbnail_url(); ?>">
+			</div>
 				<div class="page row">
 					<div class=".large-offset-1 large-6 end columns">
 						<header>
@@ -59,7 +48,7 @@ if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned
 						</header>
 					
 						<div class="entry-content">
-							<?php the_content(); ?>
+							<?php the_title(); ?>
 						</div>
 					</div>
 				</div>
